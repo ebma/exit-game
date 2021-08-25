308c269f7d93ffa12eedaefb65e59eaa67a65cea
Marcel Ebert <mail@marcel-ebert.de>
2021-08-25 20:15:41 +0200

    Remove jit flag This caused some issues with automatic rebuilding of css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: { content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], options: { safelist: [/data-theme$/] } },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -9,4 +8,13 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  // config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
 }
