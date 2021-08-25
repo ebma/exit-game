2931f81f1759aa5f0a9e64adbaf018e44cc37c9d
Marcel Ebert <mail@marcel-ebert.de>
2021-08-25 20:22:24 +0200

    Change landing page

--- src/components/SlidingDoor/index.jsx
+++ src/components/SlidingDoor/index.jsx
@@ -18,14 +18,18 @@ function SlidingDoor(props) {
           />
         ) : undefined}
 
-        <div className="curtain__panel curtain__panel--left">
-          <h1>Click to reveal&nbsp;</h1>
+        <div className="curtain__panel curtain__panel--left bg-primary">
+          <div className="pr-4 prose prose-lg">
+            <h1 className="prose">Exit</h1>
+          </div>
         </div>
 
         <div className="curtain__content">{children}</div>
 
-        <div className="curtain__panel curtain__panel--right">
-          <h1>a special reward...</h1>
+        <div className="curtain__panel curtain__panel--right bg-primary">
+          <div className="pl-4 prose prose-lg">
+            <h1 className="prose">Game</h1>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,41 +1,21 @@
-import Button from "../components/Button"
 import React from "react"
 import SlidingDoor from "../components/SlidingDoor"
-import Header from "../components/Header"
 
 function LandingPage() {
   return (
     <SlidingDoor>
-      <div className="container px-6 py-16 mx-auto">
-        <Header />
-        <div className="items-center lg:flex">
-          <div className="w-full lg:w-1/2">
-            <div className="lg:max-w-lg">
-              <h1 className="text-2xl font-semibold text-gray-800 dark:text-white lg:text-3xl">
-                Subscribe To The <span className="text-indigo-500">Newsletter</span>
-              </h1>
-
-              <p className="mt-4 text-gray-600 dark:text-gray-400">
-                be the first to knows when our <span className="font-medium text-indigo-500">Brand</span> is live
+      <div className="px-6 mx-auto">
+        <div className="min-h-screen hero">
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="text-center hero-content text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+                deleniti eaque aut repudiandae et a id nisi.
               </p>
-
-              <div className="flex flex-col mt-8 space-y-3 lg:space-y-0 lg:flex-row">
-                <input
-                  id="email"
-                  type="text"
-                  className="px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                  placeholder="Email Address"
-                />
-
-                <Button>Subscribe</Button>
-              </div>
+              <button className="btn btn-primary">Get Started</button>
             </div>
-          </div>
-
-          <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-            <Button className="px-32 py-8 bg-green-400" onClick={() => console.log("Test")}>
-              Enter
-            </Button>
           </div>
         </div>
       </div>
