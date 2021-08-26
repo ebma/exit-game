276a8f5e1b8c915fda274cb6824ebcbb07395b84
Marcel Ebert <mail@marcel-ebert.de>
2021-08-26 19:52:21 +0200

    Change TypewriterText component

--- src/components/TypewriterText/index.jsx
+++ src/components/TypewriterText/index.jsx
@@ -1,8 +1,37 @@
-import "./index.css"
+import PropTypes from "prop-types"
+import React from "react"
+import Typewriter from "typewriter-effect"
+import "./index.scss"
 
 function TypewriterText(props) {
-  const { children } = props
-  return <p>{children}</p>
+  const { children, className } = props
+
+  const stringArray = React.useMemo(() => {
+    const strings = children
+      .split(".")
+      .filter((s) => s)
+      .map((s) => s.trim() + ". ")
+    return strings
+  }, [children])
+
+  return (
+    <Typewriter
+      onInit={(typewriter) => {
+        typewriter.pauseFor(1000)
+        for (const string of stringArray) {
+          typewriter.typeString(string)
+          typewriter.pauseFor(1000)
+        }
+        typewriter.start()
+      }}
+      options={{ delay: 50, wrapperClassName: className }}
+    />
+  )
+}
+
+TypewriterText.propTypes = {
+  children: PropTypes.string.isRequired,
+  className: PropTypes.string,
 }
 
 export default TypewriterText
