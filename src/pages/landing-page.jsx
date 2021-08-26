8913ee0afc04a93154510f2d9ca2471cd4e94ab1
Marcel Ebert <mail@marcel-ebert.de>
2021-08-26 19:52:40 +0200

    Change landing page

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,19 +1,32 @@
 import React from "react"
 import SlidingDoor from "../components/SlidingDoor"
+import TypewriterText from "../components/TypewriterText"
+import RevealingText from "../components/RevealingText"
 
 function LandingPage() {
   return (
-    <SlidingDoor>
+    <SlidingDoor
+      leftPanel={
+        <div className="pr-4 prose prose-lg">
+          <RevealingText className="flex flex-col">Exit</RevealingText>
+        </div>
+      }
+      rightPanel={
+        <div className="pl-4 prose prose-lg">
+          <RevealingText delay={4}>Game</RevealingText>
+        </div>
+      }
+    >
       <div className="px-6 mx-auto">
         <div className="min-h-screen hero">
           <div className="hero-overlay bg-opacity-60"></div>
           <div className="text-center hero-content text-neutral-content">
             <div className="max-w-md">
               <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-              <p className="mb-5">
+              <TypewriterText className="mb-5 text-3xl">
                 Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
                 deleniti eaque aut repudiandae et a id nisi.
-              </p>
+              </TypewriterText>
               <button className="btn btn-primary">Get Started</button>
             </div>
           </div>
