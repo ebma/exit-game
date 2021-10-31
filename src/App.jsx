e3fe48d597db39524af113a7a36d6b856dd12d93
Marcel Ebert <mail@marcel-ebert.de>
2021-10-31 22:49:38 +0100

    Add new route for first-puzzle-page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { themeChange } from "theme-change"
 import "./App.css"
 import "./index.css"
 import ErrorPage from "./pages/error-page"
+import FirstPuzzlePage from "./pages/first-puzzle"
 import LandingPage from "./pages/landing-page"
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
     <Router>
       <Route exact path="/">
         <LandingPage />
+      </Route>
+      <Route exact path="/Solution">
+        <FirstPuzzlePage />
       </Route>
       <Route path="*">
         <ErrorPage />
