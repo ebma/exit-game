9510b9e8d594010276c24c66492281a398232c9d
Marcel Ebert <mail@marcel-ebert.de>
2021-11-13 16:20:44 +0100

    Set up typescript dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import tsConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
+  build: { sourcemap: true },
 })
